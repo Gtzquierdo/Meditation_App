0aaad53c302e09673dee411d9a2ea56de4d64b57
Gtzquierdo <gtzquierdo@gmail.com>
2020-03-01 16:01:49 -0700

    Logic-added functions for sound and animation

--- meditation.js
+++ meditation.js
@@ -8,6 +8,7 @@ const app = () => {
     const sounds = document.querySelectorAll('.sound-picker button');
     // Time Display
     const timeDisplay = document.querySelector('.time-display');
+    const timeSelect = document.querySelectorAll('.time-select button');
     // Get the length of outline
     const outlineLength = outline.getTotalLength();
     console.log(outlineLength);
@@ -16,7 +17,48 @@ const app = () => {
 
         outline.style.strokeDasharray = outlineLength;
         outline.style.strokeDashoffset = outlineLength;
-}
+
+    // Play sound
+    play.addEventListener('click', () => {
+        checkPlaying(song);
+    });
+
+    // Select sound
+    timeSelect.forEach(option => {
+        option.addEventListener('click', function(){
+            fakeDuration = this.getAttribute('data-time');
+            timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}"${Math.floor(fakeDuration % 60)}`;
+        });
+    });
+
+    //  Create a function specific to stop and play the sounds
+    const checkPlaying = song => {
+        if(song.pause){
+            song.play();
+            video.play();
+            play.src = './svg/pause.svg';
+        } else {
+            song.pause();
+            video.pause();
+            play.src = './svg/play.svg';
+        };
+    };
+
+    // We can animate the cirlce
+    song.ontimeupdate = () => {
+        let currentTime = song.currentTime;
+        let elapsed = fakeDuration - currentTime;
+        let seconds = math.floor(elapsed % 60);
+        let minutes = math.floor(elapsed / 60);
+
+
+        // Animate the circle
+        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+        outline.style.strokeDashoffset = progress;
+        // Animate the text
+        timeDisplay.textContent = `${minutes}:${seconds}}`;
+    }
+};
 
 
 

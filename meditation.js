abf9f616d13069c91fff405a7ba177afb8ade905
Gtzquierdo <gtzquierdo@gmail.com>
2020-03-01 15:28:25 -0700

    Variables written, songs/audio next

--- meditation.js
+++ meditation.js
@@ -1,5 +1,21 @@
 const app = () => {
+    const song = document.querySelector('.song');
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.vid-container video');
 
+    // Sounds
+    const sounds = document.querySelectorAll('.sound-picker button');
+    // Time Display
+    const timeDisplay = document.querySelector('.time-display');
+    // Get the length of outline
+    const outlineLength = outline.getTotalLength();
+    console.log(outlineLength);
+    // Duration
+    let fakeDuration = 600;
+
+        outline.style.strokeDasharray = outlineLength;
+        outline.style.strokeDashoffset = outlineLength;
 }
 
 
